e3b3732a66432e1bb7722677256ccddfcd6a220c
Jacob Emerick <jpemeric@gmail.com>
2014-06-16 19:53:31 -0500

    fill out comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions allow caching of matrix inversion
+## First you need to map the normal matrix to a cacheable one
+## Then you pass it to a solving one that actually does the math
 
-## Write a short comment describing this function
+
+## This first function maps a normal matrix to a cacheable one
+## As per assignment instructions
+## Creates a basic get, set, getInverse, and setInverse method
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	## Return a list of get, set, getInverse, and setInverse
 }
 
 
-## Write a short comment describing this function
+## This second function does the actual inversion math
+## If the value has already been computed, return the cached value
+## Else, compute the matrix inverse, cache, and return
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
